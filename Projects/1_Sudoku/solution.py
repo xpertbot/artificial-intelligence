9ec449d79d867c897f93bbb73a826fe7f40c7307
Roger Orantes <orantesrd@gmail.com>
2020-06-02 19:46:55 -0500

    fix: fixing history to work with pygames

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -108,7 +108,8 @@ def eliminate(values):
     for box in solved_values:
         digit = values[box]
         for peer in peers[box]:
-            values[peer] = values[peer].replace(digit,'')
+            values = assign_value(values, peer, values[peer].replace(digit,''))
+            
     return values
 
 
@@ -136,7 +137,7 @@ def only_choice(values):
         for digit in '123456789':
             dplaces = [box for box in unit if digit in values[box]]
             if len(dplaces) == 1:
-                values[dplaces[0]] = digit
+                values = assign_value(values, dplaces[0], digit)
     return values
 
 

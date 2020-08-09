d935d4eb10348be419fd0ac371a47f163d406d21
Roger Orantes <orantesrd@gmail.com>
2020-08-09 09:24:13 -0500

    adversarial-search

--- Projects/3_Adversarial Search/openingbook.py
+++ Projects/3_Adversarial Search/openingbook.py
@@ -2,7 +2,6 @@ import pickle, random, time, operator
 
 from isolation import Isolation, DebugState
 from collections import defaultdict, Counter
-from _helpers import alpha_beta_search
 
 NUM_ROUNDS = 250
 
@@ -32,6 +31,9 @@ def build_table(num_rounds=NUM_ROUNDS):
 
 # Depth 4 in specifications of project
 def build_tree(state, book, depth=4):
+    if (depth == 0):
+        debug_board = DebugState.from_state(state)
+        print(debug_board)
     #using Open Move Score instead of raw wins
     if depth <= 0 or state.terminal_test():
         return -simulate(state)
@@ -49,6 +51,64 @@ def simulate(state):
     own_liberties = state.liberties(own_loc)
     opp_liberties = state.liberties(opp_loc)
     return -1 if len(own_liberties) - len(opp_liberties) < 0 else 1
+
+def alpha_beta_search(state, player_id, depth):
+
+    def min_value(state, alpha, beta, depth):
+        """ Return the value for a win (+1) if the game is over,
+        otherwise return the minimum value over all legal child
+        nodes.
+        """
+        if state.terminal_test():
+            return state.utility(player_id)
+
+        if(depth <= 0 ): return score(state, player_id)
+
+        v = float("inf")
+        for a in state.actions():
+            v = min(v, max_value(state.result(a), alpha, beta, depth-1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    def max_value(state, alpha, beta, depth):
+        """ Return the value for a loss (-1) if the game is over,
+        otherwise return the maximum value over all legal child
+        nodes.
+        """
+        if state.terminal_test():
+            return state.utility(player_id)
+        if(depth <= 0 ): return score(state, player_id)
+
+
+        v = float("-inf")
+        for a in state.actions():
+            v = max(v, min_value(state.result(a), alpha, beta, depth-1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    alpha = float("-inf")
+    beta = float("inf")
+    best_score = float("-inf")
+    best_move = None
+    for a in state.actions():
+        v = min_value(state.result(a), alpha, beta, depth-1)
+        alpha = max(alpha, v)
+        if v >= best_score:
+            best_score = v
+            best_move = a
+    return best_move
+
+def score(state, player_id):
+    own_loc = state.locs[player_id]
+    opp_loc = state.locs[1 - player_id]
+    own_liberties = state.liberties(own_loc)
+    opp_liberties = state.liberties(opp_loc)
+    return len(own_liberties) - len(opp_liberties)
+
 
 if __name__ == "__main__":
     main()
